e06296fae38ac755907bd302321202a61548c87e
noon <nun_lovebie@hotmail.com>
2017-12-13 12:52:27 +0700

    create users

--- contact/contact.js
+++ contact/contact.js
@@ -1,5 +1,42 @@
 const express = require('express')
 const router = express()
 
-
+let contactlist = [
+    {id: 001, 
+        Firstname: 'Chutima', 
+        Lastname: 'Cherdchome', 
+        email: 'chu_1234@mail.com', 
+        URL: 'www.google.com', 
+        Note: 'Like read a book'},
+    {id: 002, 
+        Firstname: 'Kanjana', 
+        Lastname: 'Gatepan', 
+        email: 'Kikky_kan@mail.com', 
+        URL: 'www.google.com', 
+        Note: 'Play guitar'},
+    {id: 003, 
+        Firstname: 'Chonthicha', 
+        Lastname: 'Joyseekate', 
+        email: 'Pui030@mail.com', 
+        URL: 'www.google.com', 
+        Note: 'Study Com-sci'},
+    {id: 004, 
+        Firstname: 'Patcharaphon', 
+        Lastname: 'Chaladthunyakij', 
+        email: 'Googpat@mail.com', 
+        URL: 'www.google.com', 
+        Note: 'Do not like playFootball '},
+    {id: 005, 
+        Firstname: 'Chatmongkol', 
+        Lastname: 'Lintalak', 
+        email: 'mookko_005@mail.com', 
+        URL: 'www.google.com', 
+        Note: 'Play Game'},
+    {id: 006, 
+        Firstname: 'Supansa', 
+        Lastname: 'chimbanrai', 
+        email: 'mild20@mail.com', 
+        URL: 'www.google.com', 
+        Note: 'Like Korea'},
+];
 module.exports=router
